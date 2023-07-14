4a10aac8588f3a97677f2f75d8f2778cd0e7e41e
Benedicta1 <benedictaojogwu@gmail.com>
2023-07-14 00:05:27 -0700

    Write a program that prints the result of the addition of all arguments

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -5,7 +5,6 @@ if __name__ == "__main__":
     import sys
 
     total = 0
-    for i in range(len(sys.argv) - 1):
-        total += int(sys.argv[i + 1])
+    for a in range(len(sys.argv) - 1):
+        total += int(sys.argv[a + 1])
     print("{}".format(total))
-
