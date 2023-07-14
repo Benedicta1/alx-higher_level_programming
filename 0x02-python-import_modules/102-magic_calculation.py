40899425edb1f5279bc11580135762431ff4c272
Benedicta1 <benedictaojogwu@gmail.com>
2023-07-14 04:10:54 -0700

    Write the Python function def magic_calculation(a, b): that does exactly the same as the following Python bytecode

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
+from magic_calculation_102 import add, sub
 
 def magic_calculation(a, b):
-    """Match the bytecode."""
-    from magic_calculation_102 import add, sub
 
     if a < b:
         c = add(a, b)
@@ -12,4 +11,3 @@ def magic_calculation(a, b):
 
     else:
         return(sub(a, b))
-
