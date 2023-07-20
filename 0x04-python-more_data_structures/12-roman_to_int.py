d672ee03740a37e121099c42d6a3db2f88e9cd7d
Benedicta1 <benedictaojogwu@gmail.com>
2023-07-20 01:15:00 -0700

    Create a function def roman_to_int(roman_string): that converts a Roman numeral to an integer.

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if roman_string is None or type(roman_string) is not str:
+        return 0
+    integer_sum = 0
+    converter = dict(I=1, IV=4, IX=9, XLIX=49, XCIX=99, CDXCIX=499, CMXCIX=999)
+    for current, next in zip(roman_string, roman_string[1:]):
+        if converter[current] >= converter[next]:
+            integer_sum += converter[current]
+        else:
+            integer_sum -= converter[current]
+    integer_sum += converter[roman_string[-1]]
+    return integer_sum
