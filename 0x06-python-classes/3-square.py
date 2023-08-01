fe27e7f64adc33472d8761c7d0dbbd03585f421e
Benedicta1 <Benedictaojogwu@gmail.com>
2023-08-01 05:57:03 -0700

    Write a class Square that defines a square by: (based on 2-square.py)

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,17 +1,16 @@
 #!/usr/bin/python3
-"""writing an empty class"""
-
+"""writing an empty class """
 
 
 class Square:
     """this is a class defined for square"""
     def __init__(self, size=0):
-         if type(size) != int:
+        if type(size) != int:
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
         else:
             self.__size = size
 
-            def area(self):
-                return self.__size * self.__size
+    def area(self):
+        return self.__size * self.__size
