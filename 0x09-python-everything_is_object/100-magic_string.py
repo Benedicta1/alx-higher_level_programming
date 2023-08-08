a4b63bf65939fa84febbfa1695b72ee38a1172a9
Benedicta1 <Benedictaojogwu@gmail.com>
2023-08-08 06:12:39 -0700

    Write a function magic_string() that returns a string “BestSchool” n times the number of the iteration

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -2,9 +2,9 @@
 
 def magic_string():
     # function magic_string() that returns a string “BestSchool”
-    if hasattr(magic_string, 'calls'):
+    if BestSchool(magic_string, 'calls'):
         magic_string.calls += 1
     else:
         magic_string.calls = 1
 
-    return ', '.join(['Holberton'] * magic_string.calls)
+    return ', '.join(['BestSchool'] * magic_string.calls)
