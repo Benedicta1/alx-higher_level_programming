9ef354be27064bbc46c73e01884704a69f8bd83e
Benedicta1 <Benedictaojogwu@gmail.com>
2023-08-15 05:12:56 -0700

    #!/usr/bin/python3 """0x0B. Python - Input/Output, task 15. Search and update"""
    def append_after(filename="", search_string="", new_string=""):
        """Inserts a line of text to a file, after each line containing a specific
        string.
    
        Args:
            filename (str): file to search
            search_string (str): search term
            new_string (str): line to insert into file after line containing match
    
        """
        with open(filename, r+, encoding=utf-8) as curr_file:
            lines = curr_file.readlines()
            curr_file.seek(0)
            for i, line in enumerate(lines):
                #            print("read:   {}".format(line), end=)
                if search_string in line:
                    lines[i] = line + new_string
                    #                print("modded: {}".format(line), end=)
    #        for line in lines:
    #            print(line, end="")

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -1,17 +1,16 @@
 #!/usr/bin/python3
-"""Search and Update"""
+"""Search and update"""
 
 
 def append_after(filename="", search_string="", new_string=""):
-    """Inserts a line of text to a file, after each line containing a specific
-    string."""
+    """Inserts a line of text to a file"""
 
 
-    with open(filename, 'r+', encoding='utf-8') as curr_file:
-        lines = curr_file.readlines()
-        curr_file.seek(0)
+    with open(filename, 'r+', encoding='utf-8') as current_file:
+        lines = current_file.readlines()
+        current_file.seek(0)
         for b, line in enumerate(lines):
 
             if search_string in line:
                 lines[b] = line + new_string
-                curr_file.writelines(lines)
+                current_file.writelines(lines)
