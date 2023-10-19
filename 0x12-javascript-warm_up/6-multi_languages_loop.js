e3ea5acba76e787f3b554b6dde2a52055a27dddb
Benedicta1 <benedictaojogwu@gmail.com>
2023-10-19 06:53:10 -0700

    Write a script that prints 3 lines: (like 1-multi_languages.js) but by using an array of string and a loop
    The first line: “C is fun”
    The second line: “Python is cool”
    The third line: “JavaScript is amazing”
    You must use console.log(...) to print all output
    You are not allowed to use var
    You are not allowed to use any if/else statement
    You can use only one console.log
    You must use a loop (while, for, etc.)

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 !/usr/bin/node
 const msgArray = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-for (let a = 0; a < 2; a++) {
+for (let a = 0; a < 3; a++) {
   console.log(msgArray[a]);
 }
