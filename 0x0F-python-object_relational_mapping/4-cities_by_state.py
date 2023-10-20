eb01b4c5e0c48abd6dde0205bd411d5eb55987a8
Benedicta1 <benedictaojogwu@gmail.com>
2023-10-20 08:26:39 -0700

    Write a script that lists all cities from the database hbtn_0e_4_usa
    Your script should take 3 arguments: mysql username, mysql password and database name
    You must use the module MySQLdb (import MySQLdb)
    Your script should connect to a MySQL server running on localhost at port 3306
    Results must be sorted in ascending order by cities.id
    You can use only execute() once
    Results must be displayed as they are in the example below
    Your code should not be executed when imported

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,22 +1,19 @@
-!/usr/bin/python3
-"""0x0F. Python - Object-relational mapping - Fifth Task"""
+#!/usr/bin/python3
+''' script for task 4 '''
 
-if __name__ == '__main__':
-    import sys
-    import MySQLdb
 
-    if len(sys.argv) != 4:
-        print('Use: 0-select_states.py <mysql username> <mysql password>'
-              ' <database name>')
-        sys.exit()
+import sys
+import MySQLdb
 
-    conn = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1],
-                           passwd=sys.argv[2], db=sys.argv[3], charset='utf8')
-    cur = conn.cursor()
-    cur.execute('SELECT cities.id, cities.name, states.name FROM cities '
-                'LEFT JOIN states ON cities.state_id = states.id;')
-    query_rows = cur.fetchall()
-    for row in query_rows:
-        print(row)
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
+    cur = db.cursor()
+    cur.execute("""SELECT cities.id, cities.name, states.name FROM cities,
+                states WHERE cities.state_id = states.id;""")
+    states = cur.fetchall()
+    for state in states:
+        print(state)
     cur.close()
-    conn.close()
+    db.close()
