f7a1fd2694655d7251ed5602d31e6cc5ef2808ad
Benedicta1 <benedictaojogwu@gmail.com>
2023-10-23 04:11:21 -0700

    Write a script that deletes all State objects with a name containing the letter a from the database hbtn_0e_6_usa
    Your script should take 3 arguments: mysql username, mysql password and database name
    You must use the module SQLAlchemy
    You must import State and Base from model_state - from model_state import Base, State
    Your script should connect to a MySQL server running on localhost at port 3306
    Your code should not be executed when imported

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,25 +1,20 @@
 #!/usr/bin/python3
-"""0x0F. Python - Object-relational mapping - Fourteen Task
-"""
+""" deletes all states that contains the letter a """
+
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import sys
 
 if __name__ == '__main__':
-    from sys import argv, exit
-    from sqlalchemy import create_engine
-    from sqlalchemy.orm import Session
-    from model_state import Base, State
-
-    if len(argv) != 4:
-        exit('Use: 13-model_state_delete_a.py <mysql username> '
-             '<mysql password> <database name> ')
-
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/'
-                           '{}'.format(argv[1], argv[2], argv[3]),
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
-    session = Session(engine)
-    Base.metadata.create_all(engine)  # creates decprecated warning 1681
-
-    a_states = session.query(State).filter(State.name.like('%a%')).all()
-    for state in a_states:
+    Base.metadata.create_all(engine)
+    Sessionmaker = sessionmaker(bind=engine)
+    session = Sessionmaker()
+    states = session.query(State).filter(State.name.like('%a%')).all()
+    for state in states:
         session.delete(state)
     session.commit()
     session.close()
