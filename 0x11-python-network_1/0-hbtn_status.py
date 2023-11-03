c5b8f3fcb90445f9ce4c8fa0ea0558187f9aa7e2
Benedicta1 <benedictaojogwu@gmail.com>
2023-11-03 05:58:03 -0700

    Write a Python script that fetches https://alx-intranet.hbtn.io/status
    You must use the package urllib
    You are not allowed to import any packages other than urllib
    The body of the response must be displayed like the following example (tabulation before -)
    You must use a with statement

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,14 +1,13 @@
-#!/usr/bin/python3
-"""
-0. What's my status? #0
-"""
+#!/usr/bin/python
+'''
+fetches https://alx-intranet.hbtn.io/status
+'''
+import urllib.request as request
 
-if __name__ == "__main__":
-    from urllib import request
-
-    with request.urlopen('https://alx-intranet.hbtn.io/status') as response:
-        html = response.read()
-    print('Body response:')
-    print('\t- type: {}'.format(type(html)))
-    print('\t- content: {}'.format(html))
-    print('\t- utf8 content: {}'.format(html.decode('utf-8')))
+if __name__ == '__main__':
+    with request.urlopen('https://intranet.hbtn.io/status') as response:
+        content = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode('utf-8')))
