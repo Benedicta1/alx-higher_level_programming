47484dfa8c6fecf4b8fbcb938f82f67113719ba6
Benedicta1 <benedictaojogwu@gmail.com>
2023-11-03 06:05:49 -0700

    Write a Python script that fetches https://alx-intranet.hbtn.io/status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,13 +1,13 @@
-#!/usr/bin/python
-'''
-fetches https://alx-intranet.hbtn.io/status
-'''
-import urllib.request as reque
+#!/usr/bin/python3
+"""Script that fetches https://alx-intranet.hbtn.io/status
+"""
 
-if __name__ == '__main__':
-    with reque.urlopen('https://alx-intranet.hbtn.io/status') as respo:
-        content = respo.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(content)))
-        print("\t- content: {}".format(content))
-        print("\t- utf8 content: {}".format(content.decode('utf-8')))
+if __name__ == "__main__":
+    from urllib import request
+
+    with request.urlopen('https://intranet.hbtn.io/status') as respo:
+        html = respo.read()
+    print('Body response:')
+    print('\t- type: {}'.format(type(html)))
+    print('\t- content: {}'.format(html))
+    print('\t- utf8 content: {}'.format(html.decode('utf-8')))
