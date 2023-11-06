504c80c316b7759968bc0794bcc2cabcca795bb8
Benedicta1 <benedictaojogwu@gmail.com>
2023-11-06 07:41:11 -0800

    Write a Python script that takes your GitHub credentials (username and password) and uses the GitHub API to display your id

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+'''takes your GitHub credentials
+'''
+import requests
+from sys import argv
+if __name__ == "__main__":
+    response = requests.get('https://api.github.com/user',
+                            auth=(argv[1], argv[2]))
+    if "json" not in response.headers.get('content-type'):
+        print("Not a valid JSON")
+    else:
+        response = response.json()
+        print(response.get('id'))
