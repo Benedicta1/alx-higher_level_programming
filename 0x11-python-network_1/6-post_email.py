fe14a66d4fcb519b0899c470df383ef0ef0cf018
Benedicta1 <benedictaojogwu@gmail.com>
2023-11-06 07:28:46 -0800

    Write a Python script that takes in a URL and an email address, sends a POST request to the passed URL with the email as a parameter, and finally displays the body of the response.

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""request to the passed URL with the email as a parameter
+"""
+
+if __name__ == "__main__":
+    from requests import post
+    from sys import argv
+
+    response = post(argv[1], data={'email': argv[2]})
+    print(response.text)
