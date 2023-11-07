d3516964a370cab211dbc12678149f82398d2c63
Benedicta1 <benedictaojogwu@gmail.com>
2023-11-07 01:38:42 -0800

    Write a Python script that fetches https://alx-intranet.hbtn.io/status

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -2,9 +2,9 @@
 """https://alx-intranet.hbtn.io/status
 """
 
+import requests
 if __name__ == "__main__":
-    from requests import get
-
-    response = get('https://intranet.hbtn.io/status')
-    print('Body response:\n\t- type: {}\n\t- content: {}'.format(
-        type(response.text), response.text))
+    response = requests.get("https://intranet.hbtn.io/status")
+    print("Body response:")
+    print("\t- type: {}".format(type(response.text)))
+    print("\t- content: {}".format(response.text))
