628f98e81b53b3b7a15194c6464a6764ca93c073
Benedicta1 <benedictaojogwu@gmail.com>
2023-11-07 02:01:47 -0800

    Write a Python script that takes 2 arguments in order to solve this challenge.

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,20 +1,21 @@
 #!/usr/bin/python3
-"""Holberton School Interview"""
+"""Holberton School interview"""
+
+import requests
+from sys import argv
 
 if __name__ == "__main__":
-    from requests import get
-    from sys import argv
-
-    def commit_date(inter):
-        return inter.get('commit').get('author').get('date')
-
-    url = 'https://api.github.com/repos/{}/{}/commits'.format(argv[2], argv[1])
-    response = get(url)
-    commit_list = response.json()
-    list_by_date = sorted(commit_list, key=commit_date, reverse=True)
-    for i, obj_dict in enumerate(list_by_date):
-        if i > 9:
-            break
-        print('{}: {}'.format(obj_dict.get('sha'),
-                              obj_dict.get
-                                  'commit').get('author').get('name')))
+    commits = 'https://api.github.com/repos/'\
+              + argv[2] + '/' + argv[1] + '/commits'
+    response = requests.get(commits)
+    if "json" not in response.headers.get('content-type'):
+        print("Not a valid JSON")
+    else:
+        i = 0
+        response = response.json()
+        for commit in response:
+            if i > 9:
+                break
+            print(commit.get('sha') + ': ', end="")
+            print(commit.get('commit').get('author').get('name'))
+            i += 1
