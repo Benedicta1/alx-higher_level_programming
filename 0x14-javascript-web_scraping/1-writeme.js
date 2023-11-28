bd0a45499d84211fb35e88c72c92fc17fefc5d1a
Benedicta1 <benedictaojogwu@gmail.com>
2023-11-28 03:46:46 -0800

    Write a script that writes a string to a file.

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,13 +1,8 @@
 #!/usr/bin/node
 
 const fs = require('fs');
-
-let filename = '';
-if (process.argv[2] !== undefined) {
-  filename = process.argv[2];
-}
-fs.writeFile(filename, process.argv[3], 'utf8', (err) => {
-  if (err) {
-    console.log(err);
+fs.writeFile(process.argv[2], process.argv[3], function (error) {
+  if (error) {
+    console.log(error);
   }
 });
